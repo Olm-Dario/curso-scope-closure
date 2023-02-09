3f43a8856c207c5e9ff559bfd6d564a668aeab58
Dario <dariolmedo2@gmail.com>
2023-02-09 10:11:01 -0300

    ejemplo de closure

--- src/closure/lexical.js
+++ src/closure/lexical.js
@@ -18,3 +18,23 @@ function myFunction() {
 }
 
 myFunction();
+
+
+/* ------------------------------------------ */
+
+function sumWithClosure(firstNum = 0) {
+  return function (secondNum = 0) {
+    return console.log(firstNum + secondNum);
+  }
+}
+
+sumWithClosure(1)(3);
+
+/* 
+  Como se puede ver en este caso la funcion tiene doble parametro,
+  si prestas atencion el retorno de la primera funcnion devuelve una
+  funcion anonima, para este caso en particular para evitar errores
+  se pone valores por defecto en los parametro ya que si no hacemos 
+  nos daria error si llamamos la funcion y no le pasamos parametros
+  para realizar la suma
+*/
